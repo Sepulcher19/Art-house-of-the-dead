c4c19c2bfbe599278a79f1366f516486870f8c60
SR198889 <SR198889@falmouth.ac.uk>
2021-02-11 13:07:16 +0000

    added in textures and the raycast to url scripts

---
+++ Assets/Scripts/ObjectClicker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class ObjectClicker : MonoBehaviour
+{
+    private ArtProperties art;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform != null)
+                {
+                    art = hit.transform.GetComponent<ArtProperties>();
+                    print(art.url);
+                    Application.OpenURL(art.url);
+                }
+            }
+        }
+    }
+}
